88efd88981e4c46dd8c41836b02b54aa35251f6f
janstigliani <janstigli@gmail.com>
2025-04-10 09:15:23 +0200

    Refactor: Update DecksComponent to use signals for deck attributes and winner calculation

--- src/app/components/decks/decks.component.ts
+++ src/app/components/decks/decks.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { YugiService } from '../../services/yugi.service';
 import { CommonModule } from '@angular/common';
 
@@ -9,17 +9,58 @@ import { CommonModule } from '@angular/common';
   styleUrl: './decks.component.scss'
 })
 export class DecksComponent {
-selectDeck1() {
-throw new Error('Method not implemented.');
-}
-selectDeck2() {
-throw new Error('Method not implemented.');
-}
 
+  deck1Atk = signal(0);
+  deck1Def = signal(0);
+  deck2Atk = signal(0);
+  deck2Def = signal(0);
   service = inject(YugiService);
+  winner = signal("");
+
+  constructor() {
+    effect(() => {
+      this.calculateNum1()
+    })
+    effect(() => {
+      this.calculateNum2()
+    })
+  }
+
+  calculateNum1() {
+
+    for (const card of this.service.deck1()) {
+      this.deck1Atk.update(oldValue => oldValue + card.atk);
+      this.deck1Def.update(oldValue => oldValue + card.def);
+    }
+
+  }
+
+  calculateNum2() {
+    for (const card of this.service.deck2()) {
+      this.deck2Atk.update(oldValue => oldValue + card.atk);
+      this.deck2Def.update(oldValue => oldValue + card.def);
+    }
+
+  }
 
   compareDecks() {
-    throw new Error('Method not implemented.');
+    const atk1 = this.deck1Atk()-this.deck2Def();
+    const atk2 = this.deck2Atk()-this.deck1Def();
+
+    if (atk1>atk2) {
+      this.winner.set("Deck 1")
+    } else if (atk1 === atk2) {
+      this.winner.set("Draw")
+    } else {
+      this.winner.set("Deck 2")
+    }
   }
+
+  selectDeck1() {
+    this.service.deck1IsSelected = true;
+    }
+  selectDeck2() {
+    this.service.deck1IsSelected = false;
+    }
 
 }
